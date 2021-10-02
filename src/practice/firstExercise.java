88be92c15ba3a025ab4f2855c6127426e9ee69bd
shahl <shahl@192.168.1.239>
2021-10-01 21:22:36 -0400

    commit

--- src/practice/firstExercise.java
+++ src/practice/firstExercise.java
@@ -5,5 +5,14 @@ public class firstExercise {
 	public static void main(String []args) {
 		System.out.println("first excerise");
 	}
-
+	
+	int a = 5;
+	int b = 4;
+	int c = 8;
+	
+	public void addNum(int a, int b, int c) {
+	
+	System.out.println(a+b+c);
 }
+}
+
